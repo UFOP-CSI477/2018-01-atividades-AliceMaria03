6d9f8c1a8ac4bc83aafc9f7192620e40926e76c2
AliceMa03 <alicemap-@hotmail.cm>
2018-06-23 21:30:41 -0300

    alterações em atividade prática e exercicio9

---
+++ exercicios/exercicio9/resources/views/estados/create.blade.php
@@ -0,0 +1,10 @@
+@extends ('layout.principal')
+@section('conteudo')
+<h1>Inserir estado</h1>
+<form method="post" action="{{route('estados.store')}}">
+	@csrf
+	<p>Nome:<input type="text" name="nome"></p>
+	<p>Sigla:<input type="text" name="sigla"></p>
+	<p>Nome:<input type="submit" name="btnIncluir" value="Incluir"></p>
+</form>
+@endsection('conteudo')
